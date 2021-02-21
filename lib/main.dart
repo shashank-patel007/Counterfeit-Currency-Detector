3932a28e82755f3ec381732e66afbf9da65ee76d
Chaitya Shah <chaityashah22@gmail.com>
2021-02-21 15:06:10 +0530

    Scanner added

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:currency_detector_app/scanner.dart';
 import 'package:flutter/material.dart';
 import 'constants.dart';
 import 'package:camera/camera.dart';
@@ -27,6 +28,62 @@ class _HomePageState extends State<HomePage> {
   CameraController cameraController;
   List cameras;
   String imgPath;
+
+  static Future<dynamic> showMessage(m1, m2, context,
+      {bool isDimissible = true}) {
+    return showModalBottomSheet(
+        isDismissible: isDimissible,
+        context: context,
+        elevation: 12,
+        isScrollControlled: true,
+        backgroundColor: Colors.transparent,
+        builder: (BuildContext bc) {
+          return Container(
+              height: 130,
+              decoration: BoxDecoration(
+                color: Color(0xff1e1e23),
+                borderRadius: BorderRadius.circular(20.0),
+              ),
+              margin: EdgeInsets.all(10),
+              padding: EdgeInsets.all(10),
+              child: Center(
+                  child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Container(
+                    padding: EdgeInsets.symmetric(vertical: 15, horizontal: 20),
+                    decoration: BoxDecoration(color: Colors.teal, borderRadius: BorderRadius.circular(30)),
+                    child: Text(
+                      '₹ 500',
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 22,
+                          fontWeight: FontWeight.w500),
+                    )
+                  ),
+                  GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                        context, MaterialPageRoute(builder: (context) => Scanner()));
+                      },
+                      child: Container(
+                      padding: EdgeInsets.symmetric(vertical: 15, horizontal: 20),
+                      decoration: BoxDecoration(color: Colors.teal, borderRadius: BorderRadius.circular(30)),
+                      child: Text(
+                        '₹ 2000',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 22,
+                          fontWeight: FontWeight.w500),
+                      )
+                    ),
+                  )
+                  
+                  
+                ],
+              )));
+        });
+  }
 
   Future _initCameraController(CameraDescription cameraDescription) async {
     if (cameraController != null) {
@@ -89,6 +146,9 @@ class _HomePageState extends State<HomePage> {
     });
   }
 
+  int currentTab = 0;
+  final PageStorageBucket bucket = PageStorageBucket();
+
   @override
   void initState() {
     // TODO: implement initState
@@ -99,11 +159,11 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: kBackgroundColor,
+      backgroundColor: Color(0xff1e1e23),
       appBar: AppBar(
         titleSpacing: 0,
         elevation: 0,
-        backgroundColor: Colors.transparent,
+        backgroundColor: Color(0xff885566),
         centerTitle: true,
         title: Text(
           "Counterfeit Currency Detector",
@@ -114,103 +174,90 @@ class _HomePageState extends State<HomePage> {
           ),
         ),
       ),
-      body: Container(
-        child: SafeArea(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Expanded(
-                child: cameraPreviewWidget(),
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Image.asset('assets/front-2000.jpg'),
+            Image.asset('assets/back-2000.jpg')
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.crop_free),
+        backgroundColor: Color(0xffBB86FC),
+        onPressed: () {
+          showMessage(
+              "lorem",
+              "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make ",
+              context);
+        },
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      bottomNavigationBar: BottomAppBar(
+        shape: CircularNotchedRectangle(),
+        color: Color(0xff121212),
+        notchMargin: 10,
+        child: Container(
+          height: 70,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  MaterialButton(
+                    minWidth: 70,
+                    onPressed: () {
+                      setState(() {
+                        // if user taps on this dashboard tab will be active
+                        currentTab = 0;
+                      });
+                    },
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Icon(
+                          Icons.menu_rounded,
+                          size: 33,
+                          color: currentTab == 0 ? Colors.blue : Colors.grey,
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
               ),
+
+              // Right Tab bar icons
+
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  MaterialButton(
+                    minWidth: 40,
+                    onPressed: () {
+                      setState(() {
+                        // if user taps on this dashboard tab will be active
+                        currentTab = 3;
+                      });
+                    },
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Icon(
+                          Icons.help_outline_outlined,
+                          size: 33,
+                          color: currentTab == 3 ? Colors.blue : Colors.grey,
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              )
             ],
           ),
         ),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: 1,
-        backgroundColor: Colors.transparent,
-        type: BottomNavigationBarType.fixed,
-        onTap: (i) {
-          print(i);
-        },
-        items: [
-          BottomNavigationBarItem(
-            icon: Stack(
-              children: [
-                PopupMenuButton(
-                  icon: Icon(
-                    Icons.menu,
-                    color: Colors.white,
-                  ),
-                  itemBuilder: (context) => [
-                    PopupMenuItem(
-                      child: Row(
-                        children: [
-                          Icon(
-                            Icons.offline_bolt,
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Text("Item2"),
-                          ),
-                        ],
-                      ),
-                    ),
-                    PopupMenuItem(
-                      child: Row(
-                        children: [
-                          Icon(
-                            Icons.offline_bolt,
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Text("Item2"),
-                          ),
-                        ],
-                      ),
-                    ),
-                    PopupMenuItem(
-                      child: Row(
-                        children: [
-                          Icon(
-                            Icons.offline_bolt,
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Text("Item2"),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-            label: '',
-          ),
-          // BottomNavigationBarItem(
-          //   icon: IconButton(
-          //     icon: Icon(
-          //       Icons.menu,
-          //     ),
-          //     onPressed: () {
-          //       print("Menu");
-          //     },
-          //   ),
-          //   label: '',
-          // ),
-          BottomNavigationBarItem(
-            icon: IconButton(
-              icon: Icon(
-                Icons.camera,
-              ),
-              onPressed: () {
-                _onCapturePressed(context);
-              },
-            ),
-            label: 'Click',
-          ),
-        ],
       ),
     );
   }
---
+++ lib/scanner.dart
@@ -0,0 +1,165 @@
+import 'package:flutter/material.dart';
+import 'constants.dart';
+import 'package:camera/camera.dart';
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
+
+class Scanner extends StatefulWidget {
+  @override
+  _ScannerState createState() => _ScannerState();
+}
+
+class _ScannerState extends State<Scanner> {
+  CameraController cameraController;
+  List cameras;
+  String imgPath;
+
+  Future _initCameraController(CameraDescription cameraDescription) async {
+    if (cameraController != null) {
+      await cameraController.dispose();
+    }
+    cameraController =
+        CameraController(cameraDescription, ResolutionPreset.high);
+
+    cameraController.addListener(() {
+      if (mounted) {
+        setState(() {});
+      }
+    });
+
+    if (cameraController.value.hasError) {
+      print("Camera Error ${cameraController.value.errorDescription}");
+    }
+    try {
+      await cameraController.initialize();
+    } on CameraException catch (e) {
+      print(e);
+    }
+  }
+
+  Widget cameraPreviewWidget() {
+    if (cameraController == null || !cameraController.value.isInitialized) {
+      return Text(
+        'Loading',
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 20,
+          fontWeight: FontWeight.w900,
+        ),
+      );
+    }
+
+    return AspectRatio(
+      aspectRatio: cameraController.value.aspectRatio,
+      child: CameraPreview(cameraController),
+    );
+  }
+
+  void _onCapturePressed(context) async {
+    try {
+      final path =
+          join((await getTemporaryDirectory()).path, '${DateTime.now()}.png');
+
+      await cameraController.takePicture(path);
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  @override
+  void initState() {
+    // implement initState
+    super.initState();
+    availableCameras().then((availableCameras) {
+      cameras = availableCameras;
+
+      if (cameras.length > 0) {
+        _initCameraController(cameras[0]).then((void value) {});
+      } else {
+        print("No camera");
+      }
+    }).catchError((err) {
+      print(err);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: kBackgroundColor,
+      appBar: AppBar(
+        titleSpacing: 0,
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        centerTitle: true,
+        title: Text(
+          "Counterfeit Currency Detector",
+          style: TextStyle(
+            fontWeight: FontWeight.w600,
+            color: Colors.white,
+            fontSize: 23,
+          ),
+        ),
+      ),
+      body: Container(
+        child: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Expanded(
+                child: cameraPreviewWidget(),
+              ),
+            ],
+          ),
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: 1,
+        backgroundColor: Colors.transparent,
+        type: BottomNavigationBarType.fixed,
+        onTap: (i) {
+          print(i);
+        },
+        items: [
+          BottomNavigationBarItem(
+            icon: IconButton(
+              icon: Icon(
+                Icons.arrow_back_ios_outlined,
+                size: 33,
+                color: Colors.white,
+              ),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            ),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: IconButton(
+              icon: Icon(
+                Icons.camera,
+              ),
+              onPressed: () {
+                _onCapturePressed(context);
+              },
+            ),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: IconButton(
+              icon: Icon(
+                Icons.arrow_back_ios_outlined,
+                size: 33,
+                color: Colors.transparent,
+              ),
+              onPressed: () {
+                print("Menu");
+              },
+            ),
+            label: '',
+          ),
+        ],
+      ),
+    );
+  }
+}
